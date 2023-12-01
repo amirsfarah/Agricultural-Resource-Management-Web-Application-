e70423f593035c340511761420420b7a49490ab5
Amir Farah <113151914+amirsfarah@users.noreply.github.com>
2023-11-30 19:30:37 -0800

    Add files via upload

---
+++ agr_website/templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style.css">
+    <!-- The title can be dynamically set for each page -->
+</head>
+<body>
+    <header>
+        <!-- Navigation Bar -->
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="crop_management.php">Crop Management</a></li>
+                <li><a href="inventory.php">Inventory</a></li>
+                <li><a href="disease_diagnosis.php">Disease Diagnosis</a></li>
+                <li><a href="space_management.php">Space Management</a></li>
+            </ul>
+        </nav>
+    </header>
